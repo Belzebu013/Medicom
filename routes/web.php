f4e00d0a2f5b4377371a8330fc5ffe22502d4ee0
Eduardo Polidorio <polidoriodaniel@hotmail>
2024-01-14 23:57:12 -0300

    Documentado Rotas

--- routes/web.php
+++ routes/web.php
@@ -13,33 +13,68 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Rota para redirecionar a página inicial para a rota de login.
+ */
 Route::get('/', function () {
     return redirect()->route('site.login');
 });
 
+/**
+ * Rota para exibir a página de login.
+ *
+ * @param int $erro Código de erro opcional.
+ */
 Route::get('/login/{erro?}', [\App\Http\Controllers\LoginController::class, 'Index'])
     ->name('site.login');
 
+/**
+ * Rota para processar o cadastro de usuário.
+ */
 Route::post('/cadastrar', [\App\Http\Controllers\LoginController::Class,'Cadastrar'])
     ->name('site.cadastrar');
 
+/**
+ * Rota para autenticar o usuário.
+ */
 Route::post('/autenticar', [\App\Http\Controllers\LoginController::Class,'Autenticar'])
     ->name('site.autenticar');
 
+/**
+ * Grupo de rotas prefixadas para a área do sistema interno ('/app').
+ */
 Route::prefix('/app')->group(function(){
 
+    /**
+     * Rota para exibir a página inicial do aplicativo.
+     */
     Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])
         ->name('app.home');
 
+    /**
+     * Rota para efetuar o logout do usuário.
+     */
     Route::get('/sair', [\App\Http\Controllers\LoginController::class, 'sair'])
         ->name('app.sair');
 
+    /**
+     * Recurso para gerenciar pacientes.
+     */
     Route::resource('/paciente', App\Http\Controllers\PacienteController::class);
         
+    /**
+     * Recurso para gerenciar médicos.
+     */
     Route::resource('/medico', App\Http\Controllers\MedicoController::class);
 
+    /**
+     * Recurso para gerenciar consultas.
+     */
     Route::resource('/consulta', App\Http\Controllers\ConsultaController::class);
 
+    /**
+     * Recurso para gerenciar especialidades.
+     */
     Route::resource('/especialidade', App\Http\Controllers\EspecialidadeController::class);    
 });
     
